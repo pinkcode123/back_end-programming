00c94b468fd6d3c4c0c6596b991781b9d16e47d9
pinkcode123 <ilesanmiryth209@gmail.com>
2023-08-26 23:02:56 +0100

    task 3 review done

--- list-dict_if/2-sum-or_mul.py
+++ list-dict_if/2-sum-or_mul.py
@@ -2,6 +2,6 @@ num1 = int(input("enter number $ "))
 num2 = int(input("Enter number $ "))
 ans = num1 * num2
 if num1 * num2 <= 1000:
-    print(f"product of{num1}and{num2} = {ans}")
+    print(f"product of{num1} and {num2} = {ans}")
 else:
     print(f"sum of {num1} and {num2} = {num1 + num2}")
