01334e2dd970a7b5c3dd08a9ae43de6b5c9f0136
pinkcode123 <ilesanmiryth209@gmail.com>
2023-08-30 13:19:20 +0100

    my workspace

---
+++ work-practice/tutorial.py
@@ -0,0 +1,14 @@
+numbers = int(input("Enter a number $ "))
+stopvalue = int(input("Enter a number $ "))
+interval = int(input("Enter a interval $ "))
+arithmetic = (input("Enter an arithmetor +,-,x $ "))
+for x in range(1,stopvalue +1, interval):
+    if arithmetic == "+" :
+        ans = numbers + x
+        print(f"{numbers} x {x} = {ans}")
+    elif arithmetic == "-" :
+        ans = numbers - x
+        print(f"{numbers} x {x} = {ans}")
+    elif arithmetic == "x":
+        ans = numbers * x
+        print(f"{numbers} x {x} = {ans}")
