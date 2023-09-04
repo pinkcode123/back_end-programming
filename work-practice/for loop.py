a4692b523eb535a37fc6f5bc4604de6808dfcd17
pinkcode123 <ilesanmiryth209@gmail.com>
2023-09-04 06:57:17 +0100

    work in practice

--- work-practice/for loop.py
+++ work-practice/for loop.py
@@ -1,13 +1,14 @@
 data = {
-    "Gentle":"admin2",
+    "Gentle": "admin2",
     "Ruth": "123abcd",
 }
 def validate_user(name, password):
-
     name = input("enter username ")
     password = input("password ")
     if name in data:
         if password == data[name]:
             print("access granted")
+            validate_user(name, password)
+            return name
         else:
             print("access denied")
