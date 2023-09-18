d59c8554946bc4ce56bc4fa39a2b329b349f56c0
pinkcode123 <ilesanmiryth209@gmail.com>
2023-09-18 12:45:24 +0100

    learning continue

--- pyFunctions/2-fun_out.py
+++ pyFunctions/2-fun_out.py
@@ -1,18 +1,17 @@
 from random import randint
+computer_Number = randint(1, 10)
 
 
 def play_game():
-    computer_Number = randint(1, 10)
     chance = 0
     while chance < 3:
-         userNumber = int(input("Guess the Number$ ") )
-         if userNumber == computer_Number:
-             print("congratulations, you WON")
-             break
-         else:
-            print("Am sorry you lost ,try again$ ")
-            chance= +1
+        userNumber = int(input("Guess the Number$ "))
+        if userNumber == computer_Number:
+            print("congratulations, you WON")
+            break
+        else:
 
+            print("Am sorry you lost ,try again$ ")
 
 
 play_game()
